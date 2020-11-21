6a884f12c11b709a3abeb6d0d24516ed38d84ea4
Antonio Chagoya <A01731037@itesm.mx>
2020-11-21 01:57:28 -0600

    added gatsby source file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,21 +2,21 @@
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-source-filsystem`,
+      resolve: `gatsby-source-filesystem`,
       options:{
         name:`pages`,
         path:`%{_dir}/src/pages`
       },
     },
     {
-      resolve: `gatsby-source-filsystem`,
+      resolve: `gatsby-source-filesystem`,
       options:{
         name:`post`,
         path:`%{_dir}/src/post`
       },
     },
     {
-      resolve: `gatsby-source-filsystem`,
+      resolve: `gatsby-source-filesystem`,
       options:{
         name:`images`,
         path:`%{_dir}/src/images`

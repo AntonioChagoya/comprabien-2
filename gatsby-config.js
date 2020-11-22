bd833979e2c0b1224ed5d8450059190f1bf03db0
Antonio Chagoya <A01731037@itesm.mx>
2020-11-22 00:21:35 -0600

    added initial post

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,27 +5,27 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options:{
         name:`pages`,
-        path:`%{_dir}/src/pages`
+        path:`${__dirname}/src/pages`
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options:{
         name:`post`,
-        path:`%{_dir}/src/post`
+        path:`${__dirname}/src/post`
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options:{
         name:`images`,
-        path:`%{_dir}/src/images`
+        path:`${__dirname}/src/images`
       },
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
-      resolve:`gatsby-pugin-mdx`,
+      resolve:`gatsby-plugin-mdx`,
       options:{
         extension:[`.md`,`.mdx`],
         gatsbyRemarkPlugins:[
